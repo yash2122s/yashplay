fdad4094711cf77828d246ff026c6d1a1c701d62
yash2122s <yaswanthrocky1438@gmail.com>
2025-02-06 15:58:51 +0530

    Fix file structure and paths

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
         open: true
     },
     define: {
-        __API_KEY__: JSON.stringify('5bfab939b1721316a653757539b52cc4'),
-        __ACCESS_TOKEN__: JSON.stringify('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YmZhYjkzOWIxNzIxMzE2YTY1Mzc1NzUzOWI1MmNjNCIsIm5iZiI6MTczNDM2NDk1NS42MjQsInN1YiI6IjY3NjA0ZjFiY2RmY2NjYmE2ZmM0ZWYyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tWPEyzzDykuZogjns4GUwJ9oBmteE8Fnm0-YtkmgqT8')
+        'import.meta.env.VITE_API_KEY': JSON.stringify('5bfab939b1721316a653757539b52cc4'),
+        'import.meta.env.VITE_ACCESS_TOKEN': JSON.stringify('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YmZhYjkzOWIxNzIxMzE2YTY1Mzc1NzUzOWI1MmNjNCIsIm5iZiI6MTczNDM2NDk1NS42MjQsInN1YiI6IjY3NjA0ZjFiY2RmY2NjYmE2ZmM0ZWYyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tWPEyzzDykuZogjns4GUwJ9oBmteE8Fnm0-YtkmgqT8')
     },
     build: {
         outDir: 'dist',
